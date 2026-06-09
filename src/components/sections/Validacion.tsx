4a3fc7c7dbc9e1bb625f4f8159c8b216ebf59d59
ximenaiturriagac <ximena.iturriagac@gmail.com>
2026-06-09 01:07:12 -0600

    Update Validacion.tsx

--- src/components/sections/Validacion.tsx
+++ src/components/sections/Validacion.tsx
@@ -32,7 +32,7 @@ const institutions = [
   {
     name: "Tecnológico de Monterrey",
     logo: "/logo-itesm.png",
-    logoClass: "object-contain w-full h-full scale-90",
+    logoClass: "object-contain w-full h-full scale-[1]",
     containerPad: "p-3",
     desc: "A través de programas de emprendimiento, innovación y aceleración, el Tecnológico de Monterrey ha acompañado el fortalecimiento del modelo de negocio y la consolidación institucional de Biolily.",
   },
